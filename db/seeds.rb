95d41854daa350d868f5a2a56c9effb52eeddbae
Karl Entwistle <me@karlentwistle.com>
2023-03-22 22:15:42 +0000

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+PIPELINE_COUNT = 2
+DYNO_COUNT = 10 # per pipeline
+
+def pipeline_attributes(user)
+  {
+    uuid: Faker::Internet.uuid,
+    api_key: Faker::Internet.uuid,
+    user:
+  }
+end
+
+def dyno_attributes(pipeline)
+  {
+    app_id: Faker::Internet.uuid,
+    pipeline:
+  }
+end
+
+admin = User.create!(
+  email: 'admin@example.org',
+  password: '12345678',
+  admin: true
+)
+
+pipelines = Pipeline.create!(
+  Array.new(PIPELINE_COUNT) { pipeline_attributes(admin) }
+)
+
+pipelines.each do |pipeline|
+  Dyno.create!(
+    Array.new(DYNO_COUNT) { dyno_attributes(pipeline) }
+  )
+end
