f21566e61be22a274a4ba15fdff3037965657cfd
Karl Entwistle <me@karlentwistle.com>
2023-03-24 08:32:48 +0000

    Make header reusable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def header(text)
+    content_tag(:h1, class: 'mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl') do
+      content_tag(:span, class: 'text-transparent bg-clip-text bg-gradient-to-r to-purple-600 from-sky-400') do
+        text
+      end
+    end
+  end
 end
