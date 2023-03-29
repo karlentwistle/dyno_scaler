3fa5d38b1993f6cfb8b8b66a0a11bc10a641aed2
Karl Entwistle <me@karlentwistle.com>
2023-03-29 22:36:30 +0100

    Tweak styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def header(text)
-    content_tag(:h1, class: 'mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl') do
+    content_tag(:h1, class: 'text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl') do
       content_tag(:span, class: 'text-transparent bg-clip-text bg-gradient-to-r to-purple-600 from-sky-400') do
         text
       end
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
 
   def subheader(text)
-    content_tag(:h2, class: 'font-extrabold dark:text-white text-3xl') do
+    content_tag(:h2, class: 'font-extrabold dark:text-white text-2xl md:text-3xl lg:text-4xl') do
       text
     end
   end
