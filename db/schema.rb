4856a20761958ff29bdfdfd02041f6c83cb01fbb
Karl Entwistle <me@karlentwistle.com>
2023-04-02 20:06:30 +0100

    Add organisation model

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_200431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "organisations", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pipelines", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "uuid", null: false
