d2eb59eb3739fc3fecf6da98a2f46d91b4795457
Karl Entwistle <me@karlentwistle.com>
2023-04-02 20:19:09 +0100

    rails generate clearance:routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
+  resource :session, controller: 'clearance/sessions', only: [:create]
+
+  resources :users, controller: 'clearance/users', only: [:create] do
+    resource :password,
+             controller: 'clearance/passwords',
+             only: %i[edit update]
+  end
+
+  get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'clearance/users#new', as: 'sign_up'
   resources :logs, only: %i[create]
   resources :pipelines, only: %i[index show new edit create update destroy]
+
+  post '/auth/:provider/callback', to: 'omniauth_callbacks#developer'
 
   draw(:admin)
 
