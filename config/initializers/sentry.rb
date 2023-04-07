9d0090f2722ac571266a8fe0573d0351f00a8fea
Karl Entwistle <me@karlentwistle.com>
2023-04-07 11:16:46 +0100

    Disable sentry transactions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,5 +6,4 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.dsn = ENV.fetch('SENTRY_DSN')
   config.enabled_environments = %w[production]
-  config.traces_sample_rate = 1.0
 end
