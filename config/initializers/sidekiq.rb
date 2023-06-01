27179a8f8a46b054f38e49ed98f0a37a56c39413
Karl Entwistle <me@karlentwistle.com>
2023-06-01 20:53:11 +0100

    Fix RedisClient::CannotConnectError (#50)
    Adjust network Redis network timeout for [Life in the
    Cloud](https://github.com/sidekiq/sidekiq/wiki/Using-Redis#life-in-the-cloud)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,13 +3,15 @@
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV.fetch('REDIS_URL', nil),
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+    network_timeout: 5
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: ENV.fetch('REDIS_URL', nil),
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+    network_timeout: 5
   }
 end
