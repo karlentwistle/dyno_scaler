34c94d7ac860f983db15b313b025aa67ca3ed6bf
Karl Entwistle <me@karlentwistle.com>
2023-08-20 20:01:28 +0100

    Ferrum::ProcessTimeoutError:
    Browser did not produce websocket url within 10 seconds, try to increase `:process_timeout`. See https://github.com/rubycdp/ferrum#customization

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,5 +3,10 @@
 require 'capybara/cuprite'
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800])
+  Capybara::Cuprite::Driver.new(
+    app,
+    window_size: [1200, 800],
+    timeout: 10,
+    js_errors: true
+  )
 end
